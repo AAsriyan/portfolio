0c16dae8c3f4fbb59c681131a5fa8bc96ec6b4cb
AAsriyan <aasriyan@protonmail.com>
2019-08-02 21:37:16 -0400

    Created About component

---
+++ src/components/About/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './About.scss';
+
+const About = () => {
+	return (
+		<div className="about">
+			<h3>About me</h3>
+			<div>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae
+					deleniti doloremque magni iure aperiam consectetur esse, dolore dolor
+					sunt ratione? Sed mollitia facere repudiandae! Ea velit hic error
+					tempore sit!
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default About;
