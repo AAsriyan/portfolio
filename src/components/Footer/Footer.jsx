7d55b1327569b8becbab4b76a2ef7e2579b796e4
AAsriyan <aasriyan@protonmail.com>
2019-08-02 23:39:57 -0400

    Updated links in footer for socials

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,21 +10,47 @@ import {
 	faTwitter
 } from '@fortawesome/free-brands-svg-icons';
 
-import FooterBg from '../../assets/images/footer-bg.png';
+import { faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
 	return (
 		<div className="footer">
 			<h2 className="title">Want to see more?</h2>
+			<div className="up">
+				<a href="#home">
+					<FontAwesomeIcon className="return" icon={faAngleDoubleUp} />
+				</a>
+			</div>
 			<div className="social">
 				<div className="icon">
-					<FontAwesomeIcon icon={faGithub} />
+					<a
+						href="https://github.com/AAsriyan"
+						className="github-links"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<FontAwesomeIcon icon={faGithub} />
+					</a>
 				</div>
 				<div className="icon">
-					<FontAwesomeIcon icon={faLinkedin} />
+					<a
+						href="https://linkedin.com/in/aasriyan"
+						className="github-links"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<FontAwesomeIcon icon={faLinkedin} />
+					</a>
 				</div>
 				<div className="icon">
-					<FontAwesomeIcon icon={faTwitter} />
+					<a
+						href="https://twitter.com/aasriyan0"
+						className="github-links"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<FontAwesomeIcon icon={faTwitter} />
+					</a>
 				</div>
 			</div>
 		</div>
